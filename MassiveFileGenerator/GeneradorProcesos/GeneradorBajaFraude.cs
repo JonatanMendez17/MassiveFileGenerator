c9b3c586d5a6c3efb19e6764efb3fb7910bfa25b
JonattanMendez <jmendez@2dsoluciones.com.ar>
2025-06-11 16:57:00 -0300

    Ajuste de herramienta

---
+++ MassiveFileGenerator/GeneradorProcesos/GeneradorBajaFraude.cs
@@ -0,0 +1,25 @@
+﻿namespace GeneradorArchivo.GeneradorProcesos
+{
+    internal class GeneradorBajaFraude : IGenerador
+    {
+        public string GenerarLineas(Random? random)
+        {
+            var customerIntegrationId = (long)(random!.NextDouble() * 99999999999999999);
+            var reason = "FRAUDE";
+
+            return $"{customerIntegrationId:D18}|{reason}";
+        }
+
+        public string ObtenerIdentificador(Random? random)
+        {
+            var assetId = (long)(random!.NextDouble() * 999999999999999999);
+            return $"{assetId:D18}";
+        }
+
+        public string DuplicacionIdentificador => "1234567890123456789";
+
+        public int Columnas => 3;
+    }
+}
+
+
--- MassiveFileGenerator/Program.cs
+++ MassiveFileGenerator/Program.cs
@@ -1,66 +1,69 @@
-﻿namespace FileGenerator
+﻿using GeneradorArchivo.GeneradorProcesos;
+
+namespace GeneradorArchivo
 {
     internal class Program
     {
-        const int _cntRegistros = 1000;
-        const TipoProceso _tipoProceso = TipoProceso.Conciliar;
-        const string _folderSinBarritaFinal = "C:\\FileGenerator";
+        const int _cntRegistros = 10;
+        const TipoProceso _tipoProceso = TipoProceso.BajaFraude;
+        const string _guardar = "C:\\";
 
         static void Main()
         {
             var random = new Random();
 
-            // Generar líneas
+            // Generar Lineas
             var lineas = new string[_cntRegistros];
             var lineasConDuplicados = new string[_cntRegistros];
 
-            var generador = GetGeneradorPorProceso(_tipoProceso, random);
+            var generador = ObtenerGeneradorPorProceso(_tipoProceso, random);
 
             for (var i = 0; i < lineas.Length; i++)
             {
-                var identifier = generador.GetIdentifier(random);
-                var lineaSinIdentifier = generador.GenerarLinea(random);
+                var identificador = generador.ObtenerIdentificador(random);
+                var lineaSinIdentificar = generador.GenerarLineas(random);
 
-                if (generador.Columns > 1)
+                if (generador.Columnas > 1)
                 {
-                    lineasConDuplicados[i] = $"{(i > (lineasConDuplicados.Length / 2) ? generador.DuplicateIdentifier : identifier)}|{lineaSinIdentifier}";
-                    lineas[i] = $"{identifier}|{lineaSinIdentifier}";
+                    lineasConDuplicados[i] = $"{(i > (lineasConDuplicados.Length / 2) ? generador.DuplicacionIdentificador : identificador)}|{lineaSinIdentificar}";
+                    lineas[i] = $"{identificador}|{lineaSinIdentificar}";
                 }
                 else
                 {
-                    lineasConDuplicados[i] = $"{(i > (lineasConDuplicados.Length / 2) ? generador.DuplicateIdentifier : identifier)}";
-                    lineas[i] = $"{identifier}";
+                    lineasConDuplicados[i] = $"{(i > (lineasConDuplicados.Length / 2) ? generador.DuplicacionIdentificador : identificador)}";
+                    lineas[i] = $"{identificador}";
                 }
-
             }
 
-            // Cambiar la ubicación de guardado del archivo
-            File.WriteAllLines($"{_folderSinBarritaFinal}\\{_tipoProceso}_Volumetria_{_cntRegistros}_{DateTime.Now:yyyyMMddhhmmss}.txt", lineas);
-            File.WriteAllLines($"{_folderSinBarritaFinal}\\{_tipoProceso}_VolumetriaConDuplicados_{_cntRegistros}_{DateTime.Now:yyyyMMddhhmmss}.txt", lineasConDuplicados);
+            // Cambiar la ubicacion de guardadp del archivo
+            File.WriteAllLines($"{_guardar}\\{_tipoProceso}_volumetria_{_cntRegistros}_{DateTime.Now:yyyyMMddhhmmss}.txt",lineas);
+            File.WriteAllLines($"{_guardar}\\{_tipoProceso}_volumetriaConDuplicador_{_cntRegistros}_{DateTime.Now:yyyyMMddhhmmss}.txt", lineasConDuplicados);
 
-            Console.WriteLine("Archivo generado correctamente.");
         }
 
-        private static IGenerador GetGeneradorPorProceso(TipoProceso tipoProceso, Random? random)
+        private static IGenerador ObtenerGeneradorPorProceso(TipoProceso tipoProceso, Random? random)
         {
             return tipoProceso switch
             {
+                TipoProceso.BajaFraude => new GeneradorBajaFraude(),
                 _ => throw new NotImplementedException(),
             };
         }
+
     }
 
     internal interface IGenerador
     {
-        string GenerarLinea(Random? random);
-        string GetIdentifier(Random? random);
-        string DuplicateIdentifier { get; }
-        int Columns { get; }
+        string GenerarLineas(Random? randon);
+        string ObtenerIdentificador(Random? randon);
+        string DuplicacionIdentificador { get; }
+        int Columnas { get; }
     }
 
     internal enum TipoProceso
     {
-        Conciliar,
-        OtroProceso
+        BajaFraude,
+        BajaMora,
+
     }
 }
